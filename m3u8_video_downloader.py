48e37e27bae6633eaf67c9ae0a3a8787f22a86af
Luis Herrera <luis.herrera@btilab.co>
2022-10-01 16:28:29 -0500

    Feat: configurating download folder selection

--- m3u8_video_downloader.py
+++ m3u8_video_downloader.py
@@ -10,7 +10,7 @@ Window.size = (800, 600)
 Builder.load_file('boxlayouts.kv')
 # Builder.load_file('my.kv')
 
-
+# ffmpeg -protocol_whitelist tcp,tls,https -i "https://dffm5yppba5na.cloudfront.net/output/hls/coursesclassesvideos2puenteelcantordefonsecaconbajos.m3u8" -c copy video.mkv
 class MyGridLayout(Widget):
 
     url = ObjectProperty(None)
@@ -20,28 +20,42 @@ class MyGridLayout(Widget):
     def download_courses(self):
         self.ids.exito_label.text = 'Espera tu video esta descargando...'
         a = open(os.devnull, 'w')
-        url = self.ids.url.text
+        # url = self.ids.url.text
+        url = 'https://dffm5yppba5na.cloudfront.net/output/hls/coursesclassesvideos2puenteelcantordefonsecaconbajos.m3u8'
         print(url)
-        nombre = self.ids.name.text
-        print(nombre)
-        ruta = nombre+'.mp4'
-        # self.url.text = ''
-        # self.name.text = ''
-        # self.add_widget(Label(text='Procesando... ', font_size=30))
+        ruta = self.ids.name.text.split('/')
+        cd = []
+        nombre_archivo = ruta.pop()
+        newpath = os.getcwd()
+        if len(ruta) > 0:
+            print(ruta)
+            folder = '/'.join(ruta)
+            newpath = os.path.join(os.getcwd(), folder)
+            print(newpath)
+            if not os.path.exists(newpath):
+                os.makedirs(newpath)
+            cd = ['cd', folder, '&&']
+        print(nombre_archivo)
+        ruta = nombre_archivo +'.avi'
+        self.ids.url.text = ''
+        self.ids.name.text = ''
+        self.ids.exito_label.text = 'Procesando... '
         print('procesando...')
         # url = "https://dffm5yppba5na.cloudfront.net/output/hls/coursesclassesvideos2puenteelcantordefonsecaconbajos.m3u8"
-        command = ['ffmpeg', '-protocol_whitelist',
+        command = cd + ['ffmpeg', '-protocol_whitelist',
                    'file,http,https,tcp,tls,crypto',
                    '-i', url, '-c', 'copy', ruta]
-        p = subprocess.call(command, stdout=a, stderr=subprocess.STDOUT)
+        # p = subprocess.call(command, stdout=a, stderr=subprocess.STDOUT)
+        try:
+            p = subprocess.run(command, check=True, shell=True)
+            self.ids.exito_label.text = 'Descarga exitosa'
+            os.startfile(newpath or os.getcwd())
 
-        if p == 0:
-            self.ids.exito_label.text = 'Descarga exitosa'
-            os.startfile(
-                "C:/Users/luisf/Desktop/code/pyscripts/video_downloader")
-        else:
-            self.ids.exito_label.text = 'Descarga exitosa'
-        return p
+        except subprocess.CalledProcessError as e:
+            print(e)
+            self.ids.exito_label.text = 'Ocurrio un error al realizar la descarga :('
+
+
 
 
 class DownloaderApp(App):
